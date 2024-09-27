44c4fcf02582105a67748050c5f377da6fd5ce3d
NataliiaSlate <nataliia@slateteams.com>
2024-09-27 12:01:15 +0300

    Added second part of the task - parent and child components

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,11 +1,9 @@
 import { Component } from '@angular/core';
 
-import { ParentComponent } from 'app/parent/parent.component';
-
 @Component({
   selector: 'ngt-footer',
   standalone: true,
-  imports: [ParentComponent],
+  imports: [],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss',
 })
